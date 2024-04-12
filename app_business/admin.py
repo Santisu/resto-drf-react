80c67aec9cf6f86d7fc79fa1fabd208c31fbc1b1
Santisu <esantibra@gmail.com>
2024-04-12 01:12:36 -0400

    add models admin

--- app_business/admin.py
+++ app_business/admin.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
-from .models import Plato, PlatoCantidadPrecio
+from .models import Plato, PlatoCantidadPrecio, RegistroVenta, Boleta
 
 # Register your models here.
 admin.site.register(Plato)
 admin.site.register(PlatoCantidadPrecio)
+admin.site.register(RegistroVenta)
+admin.site.register(Boleta)
 
